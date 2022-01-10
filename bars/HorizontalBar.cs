ef82df99ee282256aa4dabc835fc577678e24a6c
Paolo Amosso <paolo@rebelpug.com>
2022-01-10 12:04:57 +0000

    cloned chart and bar

---
+++ bars/HorizontalBar.cs
@@ -0,0 +1,40 @@
+﻿using BehaviorDesigner.Runtime.Tasks.Unity.UnityLight;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.Scripting;
+using UnityEngine.UIElements;
+
+namespace ui.Generics
+{
+    public class HorizontalBar : VisualElement
+    {
+        private VisualElement _bar;
+        
+        #region UXML
+        [Preserve]
+        public new class UxmlFactory : UxmlFactory<HorizontalBar, UxmlTraits> { }
+
+        [Preserve]
+        public new class UxmlTraits : VisualElement.UxmlTraits { }
+        #endregion
+
+        public HorizontalBar()
+        {
+            _bar = new VisualElement();
+            _bar.AddToClassList("horizontal-bar");
+            Add(_bar);
+            UpdateBarPosition(50);
+        }
+
+        public void SetupColorBar(Color setColor, float position)
+        {
+            _bar.style.backgroundColor = new StyleColor(setColor);
+            UpdateBarPosition(position);
+        }
+
+        public void UpdateBarPosition(float position)
+        {
+            _bar.style.width = new StyleLength(new Length(position, LengthUnit.Percent));
+        }
+    }
+}
